15f3d23a6f72cce38eff3c6e5f4249c55b95b33d
Divasraj1 <vimaldivas12@gmail.com>
2022-12-10 11:47:34 +0530

    Creating cars app for keeping model and cost

---
+++ src/component/CarForm.js
@@ -0,0 +1,13 @@
+function CarForm(){
+    return <div className="car-form panel">
+        <h4 className="subtitle is-3">Add Car</h4>
+        <form>
+            <div className="field-group">
+                <div>
+                    
+                </div>
+            </div>
+        </form>
+    </div>;
+}
+export default CarForm; 
